0906d8d0ad64d80dad5d4284f84849c174651024
Aulona12 <aulonauka11@gmail.com>
2023-12-09 16:31:44 +0100

    fillimi i punes ne faqen 'cars'

---
+++ Frontend/js/cars.js
@@ -0,0 +1,12 @@
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+   }
+   function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+   }
+   
+   window.onclick = function (event) {
+    if (event.target == document.getElementById("myModal")) {
+       closeModal();
+    }
+   };
