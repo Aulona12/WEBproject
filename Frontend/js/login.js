dc54420a5bf40265cde601ae0672e7f98e20521f
Ardit04 <arditahmeti16@gmail.com>
2023-12-10 13:15:27 +0100

    validimi i loginit

--- Frontend/js/login.js
+++ Frontend/js/login.js
@@ -1,7 +1,4 @@
 
-const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
 
 registerBtn.addEventListener('click', () => {
     container.classList.add("active");
@@ -9,4 +6,56 @@ registerBtn.addEventListener('click', () => {
 
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
+});
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const passwordInput = document.getElementById('password');
+
+function isValidEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+function isValidPassword(password) {
+    return password.length >= 6;
+}
+
+function validateRegistration() {
+    const name = nameInput.value.trim();
+    const email = emailInput.value.trim();
+    const password = passwordInput.value;
+
+    if (!name || !isValidEmail(email) || !isValidPassword(password)) {
+        alert('Please enter valid name, email, and password.');
+        return false;
+    }
+
+    return true;
+}
+
+function validateLogin() {
+    const email = emailInput.value.trim();
+    const password = passwordInput.value;
+
+    if (!isValidEmail(email) || !isValidPassword(password)) {
+        alert('Please enter a valid email and password.');
+        return false;
+    }
+
+    return true;
+}
+
+registerBtn.addEventListener('click', () => {
+    if (validateRegistration()) {
+        container.classList.add("active");
+    }
+});
+
+loginBtn.addEventListener('click', () => {
+    if (validateLogin()) {
+        container.classList.remove("active");
+    }
 });
